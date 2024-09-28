7ef1ad8875ba9e3622d3f143bf5b02bf9bd9d7b5
Haiqa Qaiser <2022ee255@student.uet.edu.pk>
2024-09-28 23:16:17 +0500

    Applied but not finalized

--- LSTM models/LSTM_model.py
+++ LSTM models/LSTM_model.py
@@ -1,14 +1,31 @@
 from scaling import *
 from keras.models import Sequential
 from keras.layers import LSTM, Dense
+import matplotlib.pyplot as plt
 
 # building model
 model = Sequential()
-model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 6)))
-model.add(LSTM(units=50))
+model.add(LSTM(units=20, return_sequences=True, input_shape=(x_train.shape[1], 6)))
+model.add(LSTM(units=20))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
 
-model.fit(x_train, y_train, epochs=50, batch_size=32, validation_data=(x_test, y_test))
+model.fit(x_train, y_train, epochs=120, batch_size=32, validation_data=(x_test, y_test))
 mse = model.evaluate(x_test, y_test)
 print(f'MSE: {mse}')
+
+train_mse = model.evaluate(x_train, y_train)
+test_mse = model.evaluate(x_test, y_test)
+
+
+train_accuracy = 1 - (train_mse / (np.max(y_train) - np.min(y_train)))
+test_accuracy = 1 - (test_mse / (np.max(y_test) - np.min(y_test)))
+
+print(f'Training MSE: {train_mse}')
+print(f'Training Accuracy: {train_accuracy:.2f}')
+print(f'Testing MSE: {test_mse}')
+print(f'Testing Accuracy: {test_accuracy:.2f}')
+
+plt.plot(y_test)
+plt.plot(model.predict(x_test))
+plt.show()
